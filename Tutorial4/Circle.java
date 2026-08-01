26e427b17776d74acc3ecffabc63e02756257d8c
sharonpaulose44-design <sharonpaulose44@gmail.com>
2026-08-01 15:16:12 +0530

    Create Circle.java

---
+++ Tutorial4/Circle.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Circle {
+    double radius;
+
+    // Parameterized constructor
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    double area() {
+        return 3.14 * radius * radius;
+    }
+
+    double circumference() {
+        return 2 * 3.14 * radius;
+    }
+}
+
+public class CircleDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Radius: ");
+        double r = sc.nextDouble();
+
+        Circle c = new Circle(r);
+
+        System.out.println("Area = " + c.area());
+        System.out.println("Circumference = " + c.circumference());
+
+        sc.close();
+    }
+}
