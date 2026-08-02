741be2b21dba1f539e3f2902b35b16271980998a
sharonpaulose44-design <sharonpaulose44@gmail.com>
2026-08-02 20:20:04 +0530

    Create question6.java

---
+++ Tutorial3/question6.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SumAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int sum = 0;
+        double average;
+
+        System.out.println("Enter the elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sum = sum + arr[i];
+        }
+
+        average = (double) sum / n;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+
+        sc.close();
+    }
+}
