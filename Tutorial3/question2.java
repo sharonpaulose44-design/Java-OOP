e074a6b58bca237aba0477ad938c4869ffb47948
sharonpaulose44-design <sharonpaulose44@gmail.com>
2026-08-02 20:12:59 +0530

    Create question2.java

---
+++ Tutorial3/question2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class EvenOddCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int even = 0, odd = 0;
+
+        System.out.println("Enter the elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        System.out.println("Number of even elements = " + even);
+        System.out.println("Number of odd elements = " + odd);
+
+        sc.close();
+    }
+}
