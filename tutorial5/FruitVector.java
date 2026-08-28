f6b17323fe38f8b5b507f2bd26f26ef0609cd123
sharonpaulose44-design <sharonpaulose44@gmail.com>
2026-08-28 20:59:02 +0530

    Create FruitVector.java

---
+++ tutorial5/FruitVector.java
@@ -0,0 +1,12 @@
+import java.util.Vector;
+public class FruitVector {
+    public static void main(String[] args) {
+        Vector<String> fruits = new Vector<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("Orange");
+        fruits.add("Grapes");
+        for(String fruit : fruits)
+            System.out.println(fruit);
+    }
