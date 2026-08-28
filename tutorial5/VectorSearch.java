aface0a4a36c2757e01bfe5214cc63eb13d462ef
sharonpaulose44-design <sharonpaulose44@gmail.com>
2026-08-28 21:04:36 +0530

    Create VectorSearch.java

---
+++ tutorial5/VectorSearch.java
@@ -0,0 +1,18 @@
+import java.util.Vector;
+import java.util.Scanner;
+public class VectorSearch {
+    public static void main(String[] args) {
+        Vector<String> items = new Vector<>();
+        items.add("Pen");
+        items.add("Notebook");
+        items.add("Eraser");
+        items.add("Marker");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter item name: ");
+        String item = sc.nextLine();
+        if(items.contains(item))
+            System.out.println("Item found");
+        else
+            System.out.println("Item not found");
+        sc.close();
+    }
