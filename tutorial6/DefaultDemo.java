286315bc0f7c640117fd577eeb3efa822e46565c
sharonpaulose44-design <sharonpaulose44@gmail.com>
2026-08-28 21:10:46 +0530

    Create DefaultDemo.java

---
+++ tutorial6/DefaultDemo.java
@@ -0,0 +1,18 @@
+package OOP;
+class Animal {
+    String species;
+    Animal(String species) {
+        this.species = species;
+    }
+    void displaySpecies() {
+        System.out.println("Species: " + species);
+    }
+}
+public class DefaultDemo{
+    public static void main(String[] args) {
+        Animal a = new Animal("Lion");
+        // Direct access because both classes are in the same package
+        System.out.println("Species: " + a.species);
+        a.displaySpecies();
+    }
+}
